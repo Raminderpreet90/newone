3aa7569b5e106bc5165b330198cdbae29be113d9
Raminderpreet90 <kaurr90@student.douglascollege.ca>
2020-02-09 21:58:30 -0800

    All the files of this project

---
+++ New/src/Abc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Abc {
+	
+	public static void main(String[] args)
+	{
+		int a, b, c;
+		Scanner sc=new Scanner(System.in);
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		c=a+b;
+		System.out.println("The number is "+c);
+		
+		
+	}
+
+}
